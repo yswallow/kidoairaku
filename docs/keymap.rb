5e722e73d0f7152e6acc5acbc958aec70bec540a
yswallow <1426202+yswallow@users.noreply.github.com>
2022-04-19 18:32:03 +0900

    update keymap.rb

--- docs/keymap.rb
+++ docs/keymap.rb
@@ -1,9 +1,17 @@
+puts "running"
 # Initialize a Keyboard
 kbd = Keyboard.new
 
 kbd.init_pins(
   [ 27,26,22,2 ],   # row0, row1
   [ 4,5,6,7,8,9 ]  # col0, col1
+)
+
+kbd.add_layer :default, %i( 
+    KC_A KC_B KC_C KC_D KC_E KC_F
+    KC_G KC_H KC_I KC_J KC_K KC_L 
+    KC_M KC_N KC_O KC_P KC_Q KC_R 
+    KC_S KC_T KC_U KC_V KC_W KC_X
 )
 
 power_led = 10
@@ -26,5 +34,10 @@ rgb.effect = :swirl
 # rgb.action = :thunder
 # Append the feature. Will possibly be able to write `Keyboard#append(OLED.new)` in the future
 kbd.append rgb
+
+#via = VIA.new
+#via.rows_size = 4
+#via.cols_size = 6
+#kbd.append via
 
 kbd.start!
